4ddc80c4a37059d4cd0f24607873ed6293251e1f
farkmu45 <farukmicros@outlook.com>
2022-11-21 17:33:02 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InstitutionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('activities', ActivityController::class);
+    Route::apiResource('institutions', InstitutionController::class)->except('show');
+    Route::get('/profile', [AuthController::class, 'getProfile'])->name('get.profile');
+    Route::get('/profile', [AuthController::class, 'updateProfile'])->name('update.profile');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])->middleware('guest');
+    Route::post('login', [AuthController::class, 'login'])->middleware('guest');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+});
