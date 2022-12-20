e794b1a557192b4dfe1ffb1a206da12289210130
farkmu45 <farukmicros@outlook.com>
2022-12-20 19:25:22 +0700

    Prepare for release candidate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Institution::factory(20)->create();
-        \App\Models\User::factory(10)->create();
-        \App\Models\Activity::factory(20)->create();
+        // \App\Models\Institution::factory(20)->create();
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Activity::factory(20)->create();
 
         \App\Models\User::factory()->create([
             'name' => fake()->name(),
@@ -28,14 +28,14 @@ class DatabaseSeeder extends Seeder
             'role_id' => 2,
         ]);
 
-        \App\Models\User::factory()->create([
-            'name' => fake()->name(),
-            'email' => 'user@dispendik.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'date_of_birth' => fake()->date(),
-            'phone_number' => fake()->phoneNumber(),
-            'institution_id' => 2,
-            'role_id' => 1,
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => fake()->name(),
+        //     'email' => 'user@dispendik.com',
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'date_of_birth' => fake()->date(),
+        //     'phone_number' => fake()->phoneNumber(),
+        //     'institution_id' => 2,
+        //     'role_id' => 1,
+        // ]);
     }
 }
